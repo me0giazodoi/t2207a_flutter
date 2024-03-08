7dd49fe14b29ec764b074b325438c621ae9e1c1c
me0giazodoi <hiepga1243@gmail.com>
2024-03-08 14:59:00 +0700

    demo call api by dio

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:t2207a_flutter/screen/home/ui/home_carousel.dart';
 import 'package:t2207a_flutter/screen/home/ui/home_category.dart';
+import 'package:t2207a_flutter/screen/home/ui/popular.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HomeCarousel(),
-        HomeCategory(),
-        Text("Second section")
-      ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child:  Column(
+        children: [
+          HomeCarousel(),
+          HomeCategory(),
+          Popular()
+        ],
+      ),
     );
   }
 }
